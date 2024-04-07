9b44e27b9bc7a14656154cb755975ab555f20eea
darmoed <00beargames00@bk.ru>
2024-04-08 01:05:43 +0200

    3d HW | add player movement (Horizontal and Vertical)

---
+++ Assets/Scripts/PlayerInputReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement _playerMovement;
+
+    private PlayerInputAction _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new PlayerInputAction();
+
+        _inputActions.Player.HorizontalMovement.performed += OnHorizontalMovement;
+        _inputActions.Player.HorizontalMovement.canceled += OnHorizontalMovement;
+
+        _inputActions.Player.VerticalMovement.performed += OnVerticalMovement;
+        _inputActions.Player.VerticalMovement.canceled += OnVerticalMovement;
+
+        _inputActions.Player.OnSaySomething.performed += OnSaySomething;
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _playerMovement.SetDirection(direction, 0);
+    }
+
+    private void OnVerticalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _playerMovement.SetDirection(0, direction);
+    }
+
+    private void OnSaySomething(InputAction.CallbackContext context)
+    {
+        _playerMovement.SaySomething();
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private float _dirHorizontal;
+    private float _dirVertical;
+    private float newPositionX;
+    private float newPositionY;
+
+    public void SetDirection(float dirHorizontal, float dirVertical)
+    {
+        _dirHorizontal = dirHorizontal;
+        _dirVertical = dirVertical;
+    }
+    
+    public void SaySomething()
+    {
+        Debug.Log("Something");
+    }
+
+    private void Update()
+    {
+        newPositionX = transform.position.x;
+        newPositionY = transform.position.y;
+
+        if (_dirHorizontal != 0)
+        {
+            var deltaX = _dirHorizontal * _speed * Time.deltaTime;
+            newPositionX = transform.position.x + deltaX;
+        }
+        if (_dirVertical != 0)
+        {
+            var deltaY = _dirVertical * _speed * Time.deltaTime;
+            newPositionY = transform.position.y + deltaY;
+        }
+
+        transform.position = new Vector3(newPositionX, newPositionY, transform.position.z);
+    }
+}
